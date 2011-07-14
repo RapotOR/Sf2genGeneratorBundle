31872c76550766a75417e3fa053abe4e26f3fbce
Cedric Lahouste <cedric.lahouste@gmail.com>
2011-07-14 09:13:47 +0200

    Add web interface for CRUD generation.

---
+++ Controller/CrudController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Sf2gen\Bundle\GeneratorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+//annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
+use Sensio\Bundle\GeneratorBundle\Command\Validators;
+
+use Sf2gen\Bundle\GeneratorBundle\Form\GenerateCrudType;
+use Sf2gen\Bundle\GeneratorBundle\Entity\Crud;
+
+use Symfony\Bundle\DoctrineBundle\Mapping\MetadataFactory;
+
+/**
+ * Controller for CRUD generating
+ *
+ * @author Cédric Lahouste <cedric.lahouste@gmail.com>
+ */
+class CrudController extends Controller
+{   
+    /**
+     * @Route("/crud", name="_generator_crud")
+     * @Template()
+     */
+    public function generateAction()
+    {
+        $request = $this->get('request');
+        $formEntity = new Crud();
+        
+        $form = $this->get('form.factory')->create(new GenerateCrudType(), $formEntity);
+        
+        if ('POST' == $request->getMethod()) {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                $bundle   = $this->get('kernel')->getBundle($formEntity->bundle_name);
+                $entityClass = $this->get('doctrine')->getEntityNamespace($formEntity->bundle_name).'\\'.$formEntity->entity_name;
+                $metadata = $this->getEntityMetadata($entityClass);
+                $route_prefix = $this->getEntityMetadata($entityClass);
+                
+                $generator = new DoctrineCrudGenerator($this->get('filesystem'), $this->get('kernel')->locateResource('@SensioGeneratorBundle/Resources/skeleton/crud'));
+                $generator->generate($bundle, $formEntity->entity_name, $metadata[0], $formEntity->format, $formEntity->route_prefix, $formEntity->with_write_action);
+                
+                $request->getSession()->setFlash('notice', sprintf("CRUD based on entity '%s' has been generated.", $formEntity->entity_name));
+                return new RedirectResponse($this->generateUrl('_generator'));
+            }
+        }
+        
+        return array(
+            'form' => $form->createView()
+        );
+    }
+    
+    protected function getEntityMetadata($entity)
+    {
+        $factory = new MetadataFactory($this->get('doctrine'));
+
+        return $factory->getClassMetadata($entity)->getMetadata();
+    }    
+}
---
+++ Form/GenerateCrudType.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sf2gen\Bundle\GeneratorBundle\Form;
+
+use Symfony\Component\Form\FormBuilder;
+
+use Sf2gen\Bundle\GeneratorBundle\Entity\Bundle;
+
+/**
+ * Form for Form generating
+ *
+ * @author Cédric Lahouste <cedric.lahouste@gmail.com>
+ */
+class GenerateCrudType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('bundle_name', 'text')
+            ->add('entity_name', 'text')
+            ->add('route_prefix', 'text')
+            ->add('with_write_action', 'checkbox')
+            ->add('format', 'choice', array('choices' => Bundle::getConfigurationFormats()))
+        ;
+    }
+}
